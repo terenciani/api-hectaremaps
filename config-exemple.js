f3b6da6a199ce0a099c42a4079c6610e8fc64484
Marcelo Terenciani <marcelo.terenciani@gmail.com>
2021-03-11 09:27:19 -0300

    Ocultando arquivo de configuração

---
+++ config-exemple.js
@@ -0,0 +1,31 @@
+class Config{
+    static createConfig(){
+        global.config = {
+            port: process.env.PORT || 3000,
+            secret: "",
+            jwt:{
+                secret: "",
+                expiresIn: ''
+            },
+            db:{
+                name: "",
+                url: "",
+                username: "",
+                password: ""
+            },
+            mail:{
+                host: "",
+                port: 25,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                    user: "",
+                    pass: ""
+                },
+                tls: { rejectUnauthorized: false }
+            }
+        }
+    }
+}
+
+
+module.exports = Config.createConfig()
